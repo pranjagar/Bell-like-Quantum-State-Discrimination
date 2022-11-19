f27e7049dbbf6aee353f8951eca1fa1a972ba763
Pranjal Agarwal <pranjagar12@gmail.com>
2022-11-19 16:01:05 -0500

    new discrimination function, prelinminary, working

---
+++ scratch5.py
@@ -0,0 +1,16 @@
+import math as m
+import numpy as n
+import scipy as s
+import sympy as sym
+import pandas as p
+
+
+# Splitter_combinations_list = p.read_csv('data.csv', sep= ',', header= None)
+
+# Splitter_combinations_list = n.genfromtxt('data.csv', delimiter=',')
+# print(Splitter_combinations_list[1])
+
+def f(a = '5'):
+    return 5
+
+aa = 6

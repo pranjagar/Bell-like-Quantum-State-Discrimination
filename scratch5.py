a366a40111178e6f83a4619ee49e801244d17612
Pranjal Agarwal <pranjagar12@gmail.com>
2022-11-28 17:18:43 -0500

    not working verrsion, to be reset new

--- scratch5.py
+++ scratch5.py
@@ -12,38 +12,7 @@ import testing as t
 
 # print(t.Big_resultant[449])
 
-a = [[0, 0, 0, 0, 0, 0.0, 0.5, 0.5, -0.5, -0.5], [0, 0, 0, 0, 0, 0.707, 0.5, -0.5, 0.0, 0.0], [0.707, 0, 0, 0, 0, 0, 0, 0, -0.5, 0.5], [0, 0.5, 0.5, -0.5, 0.5, 0, 0, 0, 0, 0]]
-b = t.compare_outputs(a)
-print(b)
 
-def Discrimination(L, a = 'p'):        # L is list of ten outputs
-    nonzero_positions_list = []
-    for i in range(len(L[0])):
-        zeroes = 0
-        nonzero_position = 0
-        for j in range(len(L)):
-            if L[j][i] == 0:
-                zeroes +=1
-            elif L[j][i] != 0:
-                nonzero_position += j
-            print(zeroes,nonzero_position)
-        print('----------------')
-        if zeroes == 3:
-            nonzero_positions_list.append(nonzero_position+1)    # +1 is to have discrimination 1,2,3,4 instead of 0,1,2,3
-        print(nonzero_positions_list)
-        print('----------------')
-    # print(nonzero_positions_list)
-    discriminated_bell_states = []
-    for k in range(len(nonzero_positions_list)):
-        if nonzero_positions_list[k] not in discriminated_bell_states:
-            discriminated_bell_states.append(nonzero_positions_list[k])
-    if a == 'nonzero':
-        return nonzero_positions_list
-    else:
-        return discriminated_bell_states
-
-
-print(Discrimination(a))
 
 
 

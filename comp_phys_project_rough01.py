263bb5b0639a61fa42f6d580a1bebea7c80bd4fb
Pranjal Agarwal <pranjagar12@gmail.com>
2022-12-17 20:49:26 -0500

    project, working

--- comp_phys_project_rough01.py
+++ comp_phys_project_rough01.py
@@ -81,18 +81,11 @@ def Toss_(x):
 # print(Toss_(.5))
 
 
-matrix = '12'
 
-matrix_index_1 = int(matrix[0])
-matrix_index_2 = int(matrix[1])
-
-
-input_state = [1,0,0,1]
-reduced_input_state = [input_state[matrix_index_1-1],input_state[matrix_index_2-1]]
 
 outs = [[1,0],[0,1]]
 
-def MatrixAction(reducedstate, phi= m.pi/4):                # reduced state is either [1,0] or [0,1]. phi is splitter angle
+def MatrixAction_onephoton_reduced(reducedstate, phi= m.pi/4):                # reduced state is either [1,0] or [0,1]. phi is splitter angle
     t = m.cos(phi)
     r = m.sin(phi)
     prob_t = abs(t**2)
@@ -118,18 +111,18 @@ def MatrixAction(reducedstate, phi= m.pi/4):                # reduced state is e
 
 
 
-def full_MatrixAction(reducedstate, phi= m.pi/4):                # reduced state is either [1,0] or [0,1]. phi is splitter angle
+def MatrixAction_reduced_complete(reducedstate, phi= m.pi/4):                # reduced state is either [1,0] or [0,1]. phi is splitter angle
     if reducedstate == [1,0] or reducedstate == [0,1]:
-        out = MatrixAction(reducedstate, phi)
+        out = MatrixAction_onephoton_reduced(reducedstate, phi)
     elif reducedstate == [2,0]:
-        out_1 = MatrixAction([1,0],phi)
-        out_2 = MatrixAction([1,0],phi)
+        out_1 = MatrixAction_onephoton_reduced([1,0],phi)
+        out_2 = MatrixAction_onephoton_reduced([1,0],phi)
     elif reducedstate == [0,2]:
-        out_1 = MatrixAction([0,1],phi)
-        out_2 = MatrixAction([0,1],phi)
+        out_1 = MatrixAction_onephoton_reduced([0,1],phi)
+        out_2 = MatrixAction_onephoton_reduced([0,1],phi)
     elif reducedstate == [1,1]:
-        out_1 = MatrixAction([1,0],phi)
-        out_2 = MatrixAction([0,1],phi)
+        out_1 = MatrixAction_onephoton_reduced([1,0],phi)
+        out_2 = MatrixAction_onephoton_reduced([0,1],phi)
     
     if reducedstate == [2,0] or reducedstate == [0,2] or reducedstate == [1,1]:
         if out_1 == [1,0] and out_2==[1,0]:
@@ -145,43 +138,89 @@ def full_MatrixAction(reducedstate, phi= m.pi/4):                # reduced state
 
 # print(full_MatrixAction([2,0], m.pi/15))
 
-counter = 0
-counter2 = 0
-counter3 = 0
-counter4 = 0
-counter5 = 0
-counter6 = 0
-
-for i in range(1000):
-
-    # x1 = MatrixAction([1,0], .5)
-
-    x2 = full_MatrixAction([1,1], m.pi/3)
-    print(x2)
-    # print(f' and {x2}')
-    # if x1 == [1,0]:
-        # counter+=1
-    if x2 == [1,1]:
-        counter2+=1
-    elif x2 == [2,0]:
-        counter3+=1
-    elif x2 == [0,2]:
-        counter4+=1
-    elif x2 == [1,0]:
-        counter5+=1
-    elif x2 == [0,1]:
-        counter6+=1
-
+counter,counter2,counter3,counter4,counter5,counter6,counterelse = 0,0,0,0,0,0,0
     
 # print(counter)
-print([counter3,counter4,counter2, counter5,counter6, (counter2+counter3+counter4)])
+# print([counter3,counter4,counter2, counter5,counter6, (counter2+counter3+counter4)])
+#
 
 
+def MatrixAction_full(matrix_index, inputstate, phi):            # matrixindex is a string like '12' etc, inputstate is a list like [1,0,0,1]
+    matrix_index_12_1 = int(matrix_index[0])
+    matrix_index_12_2 = int(matrix_index[1])
+    reduced_inputstate = [inputstate[matrix_index_12_1-1],inputstate[matrix_index_12_2-1]]
+    reduced_out = MatrixAction_reduced_complete(reduced_inputstate, phi)
+    full_out = [i for i in inputstate]
+    full_out[matrix_index_12_1-1] = reduced_out[0]
+    full_out[matrix_index_12_2-1] = reduced_out[1]
+    return full_out
 
 
+ 
+def SystemAction(inputstate ,splitter_comb):                    # inputstate is a vector like [1,0,0,1], splitter_comb is a list of six splitter angles
+    M12_out = MatrixAction_full('12', inputstate, splitter_comb[0])
+    M13_out = MatrixAction_full('13', M12_out, splitter_comb[1])
+    M14_out = MatrixAction_full('14', M13_out, splitter_comb[2])
+    M23_out = MatrixAction_full('23', M14_out, splitter_comb[3])
+    M24_out = MatrixAction_full('24', M23_out, splitter_comb[4])
+    M34_out = MatrixAction_full('34', M24_out, splitter_comb[5])
+    print([M12_out,M13_out,M14_out,M23_out,M24_out,M34_out])
+    return M34_out
+
+splitters = [m.pi/4,m.pi/4,m.pi/4,m.pi/4,m.pi/4,m.pi/4]  
+    
+# loop for checking probabilities
+for i in range(10):
+    x2 = MatrixAction_full('34',[1,0,1,0],splitters[0])
+    print(f' and {x2}')
+    if x2 == [1,0,1,0]:
+        counter+=1
+    elif x2 == [0,0,2,0]:
+        counter2+=1
+    elif x2 == [2,0,0,0]:
+        counter3+=1
+    elif x2 == [0,1,0,0]:
+        counter4+=1
+    elif x2 == [1,0,0,0]:
+        counter5+=1
+    elif x2 == [1,0,0,1]:
+        counter6+=1
+    else:
+        counterelse +=1
+
+print([counter, counter2,counter3,counter4,counter5,counter6,counterelse])
 
 
+""" 
+counter = [0,0,0,0,0,0,0]
+    
+for i in range(1000):
+    x2 = MatrixAction_reduced_complete([1,1],splitters[0])
+    print(f' and {x2}')
+    if x2 == [1,0]:
+        counter[0] += 1 
+    elif x2 == [0,1]:
+        counter[1] += 1 
+    elif x2 == [2,0]:
+        counter[2] += 1 
+    elif x2 == [0,2]:
+        counter[3] += 1 
+    elif x2 == [1,1]:
+        counter[4] += 1 
+    # elif x2 == [0,1]:
+    #     counter[5] += 1 
+    else:
+        counter[6] += 1 
 
+    
+print(counter)
+"""
+    
+    
+    
+    
+    
+    
 
 
 

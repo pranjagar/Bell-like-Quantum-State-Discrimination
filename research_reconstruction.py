dcf973803df1228ab8b0048627c067bf9287bc9d
Pranjal Agarwal <pranjagar12@gmail.com>
2022-12-30 18:17:16 -0500

    working edit 02

--- research_reconstruction.py
+++ research_reconstruction.py
@@ -13,16 +13,19 @@ bell_V = [fn.phiplus_V,fn.phiminus_V,fn.psiplus_V,fn.psiminus_V]
 bell_C = [fn.phiplus_C,fn.phiminus_C,fn.psiplus_C,fn.psiminus_C]
 
 
-y = [12,13,14,23,24,34]
-angles = [sym.pi/2,0,sym.pi/4,sym.pi/4,sym.pi/4,sym.pi/2]
+mirror_list = [12,13,14,23,24,34]
 
 
 #Chekcs out:  phiplus
+
+
 
 def radians(degrees):           # degrees  to radians
     rad = (sym.pi/180)*degrees
     return rad 
 
+
+#######_Definitions_######
 
 theta = sym.symbols('theta')
 theta = 45                                          # theta in degrees, default 45 gives Bell states
@@ -40,13 +43,46 @@ psiminus_like_C = [sym.sin(radians(theta)), -sym.cos(radians(theta))]
 bell_like_V = [phiplus_like_V,phiminus_like_V,psiplus_like_V,psiminus_like_V]           # making lists of bell-like vectos (and coeffs) so easier to change them as arguments later
 bell_like_C = [phiplus_like_C,phiminus_like_C,psiplus_like_C,psiminus_like_C]
 
+
+
+
+
 zz = fn.SystemAction(bell_V[0],bell_C[0], [0,sym.pi/2,sym.pi/4,0,0,0], 34, True)[1]
 
-mirr0 = Data.big_phi_abstract[100]
-dd = fn.SystemAction(bell_V[0],bell_C[0], mirr0, 34, True)[1]
-print(dd)
-# print(fn.AvgProbability([zz,zz,zz,zz]))
+mirr = Data.big_phi_abstract[100]
 
+def four_list(splitter_comb, Bell_or_BellLike = 'B', rounding = False):     # splitter comb is a six-list of the six splitters, change to 'BL' for bell like, change to True for rounding 
+    if Bell_or_BellLike == 'B':
+        Bell_V = bell_V
+        Bell_C = bell_C
+    else:
+        Bell_V = bell_like_V
+        Bell_C = bell_like_C
+    four_list = []
+    for i in range(4):
+        bell_out_C = fn.SystemAction(Bell_V[i],Bell_C[i], splitter_comb, 34, rounding)[1]
+        four_list.append(bell_out_C)
+    return four_list
+
+
+
+# dd = fn.SystemAction(bell_V[0],bell_C[0], mirr, 34, True)[1]
+# print(dd)
+# print(four_list(mirr))
+# print(fn.AvgProbability(four_list(mirr)))
+
+
+good_avg_prob_choices = []
+best_avg_prob_choices = []
+for i in range(len(Data.big_phi_abstract)):
+    mirr = Data.big_phi_abstract[i]
+    avg = fn.rounding([fn.AvgProbability(four_list(mirr))])[0]
+    if avg >= .24:
+        good_avg_prob_choices.append([avg,i])
+        if avg > .49:
+            best_avg_prob_choices.append([avg,i])
+            
+# print(best_avg_prob_choices)                      # works. gives list of pairs [avg,i] where avg is the avg prob of the four list corresp to the ith splitter comb of the splitter comb list in Data.py 
 
 
 

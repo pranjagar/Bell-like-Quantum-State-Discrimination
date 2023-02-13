0edcfce27da896b5f75056e0614797057961b9b9
Pranjal Agarwal <pranjagar12@gmail.com>
2023-02-13 10:54:46 -0500

    new jupyter notebooks etc, 02/13/23

--- research_reconstruction.py
+++ research_reconstruction.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 import math as m
 import sympy as sym
 import random as rand
@@ -51,7 +50,7 @@ zz = fn.SystemAction(bell_V[0],bell_C[0], [0,sym.pi/2,sym.pi/4,0,0,0], 34, True)
 
 mirr = Data.big_phi_abstract[100]
 
-def four_list(splitter_comb, Bell_or_BellLike = 'B', rounding = False):     # splitter comb is a six-list of the six splitters, change to 'BL' for bell like, change to True for rounding 
+def four_list(splitter_comb, Bell_or_BellLike = 'B', rounding = False, compared = False):     # splitter comb is a six-list of the six splitters, change to 'BL' for bell like, change to True for rounding, compared to True if want in output-by-output comparison format 
     if Bell_or_BellLike == 'B':
         Bell_V = bell_V
         Bell_C = bell_C
@@ -62,8 +61,11 @@ def four_list(splitter_comb, Bell_or_BellLike = 'B', rounding = False):     # sp
     for i in range(4):
         bell_out_C = fn.SystemAction(Bell_V[i],Bell_C[i], splitter_comb, 34, rounding)[1]
         four_list.append(bell_out_C)
-    return four_list
-
+    compared_four_list = [[four_list[i][j] for i in range(4)] for j in range(10)]
+    if compared == False:
+        return four_list
+    else:
+        return compared_four_list
 
 
 # dd = fn.SystemAction(bell_V[0],bell_C[0], mirr, 34, True)[1]
@@ -74,7 +76,7 @@ def four_list(splitter_comb, Bell_or_BellLike = 'B', rounding = False):     # sp
 
 good_avg_prob_choices = []
 best_avg_prob_choices = []
-for i in range(len(Data.big_phi_abstract)):
+for i in range(2978,len(Data.big_phi_abstract)):
     mirr = Data.big_phi_abstract[i]
     avg = fn.rounding([fn.AvgProbability(four_list(mirr))])[0]
     if avg >= .24:
@@ -82,20 +84,20 @@ for i in range(len(Data.big_phi_abstract)):
         if avg > .49:
             best_avg_prob_choices.append([avg,i])
             
-# print(best_avg_prob_choices)                      # works. gives list of pairs [avg,i] where avg is the avg prob of the four list corresp to the ith splitter comb of the splitter comb list in Data.py 
+print(f'Best avg prob choices = {best_avg_prob_choices}')                      # works. gives list of pairs [avg_prob,i] where avg is the avg prob of the four list corresp to the ith splitter comb of the splitter comb list in Data.py 
+
+# choice = 2230
+# print(f'choice {choice} : {four_list(Data.big_phi_abstract[choice], rounding = True)}')
+# print(f'choice {choice} compared : {four_list(Data.big_phi_abstract[choice], rounding = True, compared = True)}')
+
+
+# about first one third of the best choices, each with avg_prb 0.5 = [886,888,896,898,930,932,934,940,942,944,980,982,984,990,992,994,1136,1138,1146,1148,1180,1182,[ 1667], 1669,1670,1671,1672,1673,1674,1676,1678,1700,1702,1704,1705,1707,1709,1710,1711,1712,1713,1714,1715,1717,1719,1720,1721,1722,1723,1724,1726,1728,2136,
+# 2138,2146,2148,2180,2182,2184,2190,2192,2194,2230,2232,2234,2240,2242,2244,2386,2388,2396,2398,2430,2432,2434,2440,2442,2444,2480,2482,2484,2490,2492,2494,2626,2628,2650,2652,2654,2655,2657,2659,2660,2661,2662,2663,2664,2665,2667,2669,2670,2671,2672,2673,2674,2676,2678,2700,2702,2704,2705,2707,2709,2710,2711,2712,2713,2714,
+# 2715,2717,2719,2720,2721,2722,2723,2724,2726,2728,2876,2878,2900,2902,2904,2905,2907,2909,2910,2911,2912,2913,2914,2915,2917,2919,2920,2921,2922,2923,2924,2926,2928,2950,2952,2954,2955,2957,2959,2960,2961,2962,2963,2964,2965,2967,2969,2970,2971,2972,2973,2974,2976,2978]
 
 
 
-
-
-
-
-
-
-
-
-
-
+#[1405,1407,1409,1411,1413,1415,1417,1419,1421,1423,1455,1457,1459,1461,1463,1465,1467,1469,1471,1473,1655,1657,1659,1661,1663,1665,1667,1669,1671,1673,1705,1707,1709,1711,1713,1715,1717,1719,1721,1723,2655,2657,2659,2661,2663,2665,2667,2669,2671,2673,2705,2707,2709,2711,2713,2715,2717,2719,2721,2723,2905,2907,2909,2911,2913,2915,2917,2919,2921,2923,2955,2957,2959,2961,2963,2965,2967,2969,2971,2973,7655,7657,7659,7661,7663,7665,7667,7669,7671,7673,7705,7707,7709,7711,7713,7715,7717,7719,7721,7723,7905,7907,7909,7911,7913,7915,7917,7919,7921,7923,7955,7957,7959,7961,7963,7965,7967,7969,7971,7973,8905,8907,8909,8911,8913,8915,8917,8919,8921,8923,8955,8957,8959,8961,8963,8965,8967,8969,8971,8973,9155,9157,9159,9161,9163,9165,9167,9169,9171,9173,9205,9207,9209,9211,9213,9215,9217,9219,9221,9223,13905,13907,13909,13911,13913,13915,13917,13919,13921,13923,13955,13957,13959,13961,13963,13965,13967,13969,13971,13973,14155,14157,14159,14161,14163,14165,14167,14169,14171,14173,14205,14207,14209,14211,14213,14215,14217,14219,14221,14223,15155,15157,15159,15161,15163,15165,15167,15169,15171,15173,15205,15207,15209,15211,15213,15215,15217,15219,15221,15223,15405,15407,15409,15411,15413,15415,15417,15419,15421,15423,15455,15457,15459,15461,15463,15465,15467,15469,15471,15473]
 
 
 
